ebcfb9a8ce549b22f3ef21d9664744b2bdaa3200
SabrinaMouginot <s.mouginot@yahoo.fr>
2023-01-14 14:47:59 +0100

    création fichier script séparé

---
+++ script.js
@@ -0,0 +1,16 @@
+alert("chargement du nouveau contenu");
+document.open();
+document.write("<h1>Ma liste de tâches</h1>");
+document.write("<ol><li class=li> Xyz </li><li> Xzzeffrzfrf </li></ol>");
+
+document.write("<input type=text name=champ placeholder= Nouvelle&nbsp;tâche>&nbsp;<input type=button value=valider>");
+
+document.write("<br>&nbsp;</br>");
+document.write("Laver la voiture");
+document.write("<input type=button value=modifier>&nbsp;<input type=button value=supprimer>");
+
+document.write("<br>&nbsp;</br>");
+document.write("<del>sortir les poubelles</del>");
+document.write("<input type=button value=modifier>&nbsp;<input type=button value=supprimer>");
+
+document.close();
