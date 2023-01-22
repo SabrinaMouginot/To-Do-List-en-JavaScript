8c91dadd9116820c12b2181ec65c575fb9540b39
SabrinaMouginot <s.mouginot@yahoo.fr>
2023-01-22 23:39:31 +0100

    ajout d'une ligne

--- script.js
+++ script.js
@@ -9,16 +9,16 @@ document.write("<input type=text name=champ placeholder= Nouvelle&nbsp;tâche>&n
 
 document.write("<br>&nbsp;</br>");
 document.write("Laver la voiture");
-document.write("<input type=button value=modifier>&nbsp;<input type=button value=supprimer>");
+document.write("<input class='modifier' type=button value=modifier>&nbsp;<input class='supprimer' type=button value=supprimer>");
 
 document.write("<br>&nbsp;</br>");
 document.write("<del>sortir les poubelles</del>");
-document.write("<input type=button value=modifier>&nbsp;<input type=button value=supprimer>");
+document.write("<input class='modifier' type=button value=modifier>&nbsp;<input class='supprimer' type=button value=supprimer>");
 
 // document.close();
 
 //tableau des tâches
-var taches = []
+// let taches = []
 
 // Classe des tâches  >>>>>>>>>>>> Une classe pour instancier des tâches et un tableau pour les stocker.
 // class taches {
@@ -30,4 +30,16 @@ var taches = []
 
 // Dictionnaire des tâches  >>>>>>>>>>>>>> Un dictionnaire est une collection de clés et valeurs qui peuvent être de n’importe quel type ce qui nous convient très bien.
 let taches = new Map()
-//La seule chose qu’on ne pourra pas faire c’est modifier le texte de la tâche puisque c’est la clé. On pourrait supprimer cette entrée et en créer une autre mais alors elle se retrouverait à la fin de la liste ce qui ne serait pas très judicieux.
+//La seule chose qu’on ne pourra pas faire c’est modifier le texte de la tâche puisque c’est la clé. 
+
+
+
+// Boutons
+const buttonDel = "<button class='supprimer'>Supprimer</button>"
+const buttonsComplete = "${buttonDel}<button class='demarquer'>Démarquer</button>"
+const buttonsNoComplete = "${buttonDel}<button class='marquer'>Marquer</button>"
+
+// Ajoute une ligne de tableau
+const creerLine = (text, complete) => {
+
+}
